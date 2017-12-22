ee80f0d0c7cd149aa8801c48d5fe7525e5dd3c84
Ryan-Campbell-PT <ryacampbell15@gmail.com>
2017-12-22 16:44:26 -0600

    Basic hash structure implemented

--- Misc/HashTable.cpp
+++ Misc/HashTable.cpp
@@ -1,5 +1,5 @@
 #include "HashTable.h"
-
+#include <iostream>
 
 
 HashTable::HashTable()
@@ -9,4 +9,31 @@ HashTable::HashTable()
 
 HashTable::~HashTable()
 {
+}
+
+vNode::vNode(int ID)
+	:head(0), below(0), hashID(ID) {}
+
+vNode::~vNode()
+{
+	hNode *iter(head), *next(0);
+
+	while (iter != 0)
+	{
+		next = iter->getNext();
+		delete iter;
+		iter = next;
+	}
+}
+
+hNode::hNode(const char * title)
+	:next(0)
+{
+	bookTitle = new char[strlen(title) + 1]; //allocate necessary space
+	strcpy_s(bookTitle, strlen(title) + 1, title); //copy into buffer
+}
+
+hNode::~hNode()
+{
+	delete bookTitle;
 }
--- Misc/HashTable.h
+++ Misc/HashTable.h
@@ -1,8 +1,44 @@
-#pragma once
+
+//a hash table implementation using storing books as an example to sort
+
+class vNode;
+class hNode;
+
 class HashTable
 {
+	vNode *head;
+
 public:
 	HashTable();
 	~HashTable();
+
+	void addData(const char *bookTitle);
 };
 
+//this is the class that represents the "verticle" nodes, the ones that get the hash id
+class vNode
+{
+	
+	hNode *head; //each node has a horizontal node that is however many nodes the node holds
+	vNode *below; //each vNode has a node thats below where it is currently 
+	int hashID;
+
+public:
+	//no default constructor, if a vNode is being created, it needs a hashID
+	vNode(int ID);
+	~vNode();
+};
+
+//this is the class that represents the "horizontal" nodes, the ones that actually own the data
+class hNode
+{
+	hNode *next; //each hNode has a next pointer to the next item in line
+	char *bookTitle;
+
+public:
+	//no default constructor, if youre making a hNode, it has to come with a book title
+	hNode(const char *title);
+	~hNode();
+	
+	hNode *getNext() { return next; }
+};
