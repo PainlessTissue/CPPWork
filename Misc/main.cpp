2fc041d762e726d6ae3d47e71fa2131fab350610
Ryan-Campbell-PT <ryacampbell15@gmail.com>
2017-12-22 16:19:24 -0600

    HashTable class

--- Misc/main.cpp
+++ Misc/main.cpp
@@ -10,12 +10,70 @@
 #include <stdlib.h>
 #include <crtdbg.h>
 
+template <typename T>
+struct LL
+{
+	LL* next, *prev;
+	T item;
+
+	LL()
+		:next(0), prev(0), item(0) {}
+
+	void erase(LL<T> *head)
+	{
+		LL<T> *iter = head;
+		LL<T> *nex = 0;
+
+		while (iter != 0)
+		{
+			nex = iter->next;
+
+			delete iter;
+
+			iter = nex;
+		}
+	}
+
+	~LL<T>()
+	{
+		delete item;
+	}
+};
+
+struct obj
+{
+	int thing;
+
+	obj()
+		:thing(0) {}
+};
+
+struct newObj
+{
+	bool nay;
+
+	newObj()
+		:nay(false) {}
+};
 
 int main()
 {
-	String d("finger");
+	//obj
+	LL<obj*> *head = new LL<obj*>;
+	head->item = new obj;
 
-	d += " me daddy";
+	head->next = new LL<obj*>;
+
+	head->erase(head);
+
+	LL<newObj*> *hHead = new LL<newObj*>;
+	hHead->item = new newObj;
+
+	hHead->next = new LL<newObj*>;
+	hHead->next->item = new newObj;
+	//hHead->next->item->nay = true;
+
+	head->erase(head);
 
 	_CrtDumpMemoryLeaks();
 }
