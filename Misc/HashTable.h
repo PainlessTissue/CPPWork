419b3ab6bc9e9726eb7eedba82c36c8cc97d5724
Ryan-Campbell-PT <ryacampbell15@gmail.com>
2018-01-02 15:13:32 -0600

    Fixed mutators/accessors

--- Misc/HashTable.h
+++ Misc/HashTable.h
@@ -31,18 +31,18 @@ class vNode
 	hNode *hHead; //each node has a horizontal node that is however many nodes the node holds
 	vNode *below; //each vNode has a node thats below where it is currently 
 	int hashID;
-
-	void addNode(const char *bookTitle);
-
-	vNode *getNext() { return below; }
-	int getId() { return hashID; }
-	void setNext(vNode *next) { this->below = next; }
-
+	
 public:
 	//no default constructor, if a vNode is being created, 
 	//it needs a hashId for the node itself, and a book title for its hNode
 	vNode(int ID, const char *bookTitle);
 	~vNode();	
+
+	int getId() { return hashID; }
+	vNode *getNext() { return below; }
+	void setNext(vNode *next) { this->below = next; }
+	void addNode(const char *bookTitle);
+
 };
 
 //this is the class that represents the "horizontal" nodes, the ones that actually own the data
@@ -51,11 +51,12 @@ class hNode
 	hNode *next; //each hNode has a next pointer to the next item in line
 	char *bookTitle; //and the hNodes store the actual book title
 
-	hNode *getNext() { return next; }
-	void setNext(hNode *next) { this->next = next; }
-
+	
 public:
 	//no default constructor, if youre making a hNode, it has to come with a book title
 	hNode(const char *title);
 	~hNode();
+
+	hNode *getNext() { return next; }
+	void setNext(hNode *next) { this->next = next; }
 };
